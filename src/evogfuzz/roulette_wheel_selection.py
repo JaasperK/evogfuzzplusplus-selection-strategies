5d4c74830e1632e325b6080e81c74e8abaee46f7
Martin Kunz <kunzmart@gruenau1.informatik.hu-berlin.de>
2024-07-10 19:32:54 +0200

    Black and add comments

--- src/evogfuzz/roulette_wheel_selection.py
+++ src/evogfuzz/roulette_wheel_selection.py
@@ -4,18 +4,40 @@ import numpy as np
 
 
 class Roulette:
+    """
+    Implements the roulette wheel selection strategy for selecting fittest individuals from a given set of inputs.
+
+    Attributes:
+        test_inputs (Set[Input]): A set of input individuals to select from.
+        tournament_rounds (int): Number of rounds for the selection process.
+        tournament_size (int): Size of each selection round.
+        total_fitness (float): Total fitness of all input individuals combined, used to determine selection probabilities.
+    """
+
     def __init__(
         self,
         test_inputs: Set[Input],
         tournament_rounds: int = 10,
         tournament_size: int = 10,
     ) -> None:
+        """
+        Initialize the Roulette selection strategy.
+
+        :param test_inputs: A set of input individuals to select from.
+        :param tournament_rounds: Number of rounds for the selection process.
+        :param tournament_size: Size of each selection round.
+        """
         self.test_inputs: Set[Input] = test_inputs
         self.tournament_rounds: int = tournament_rounds
         self.tournament_size: int = tournament_size
         self.total_fitness: float = sum(input.fitness for input in test_inputs)
 
     def select_fittest_individuals(self) -> Set[Input]:
+        """
+        Select the fittest individuals based on the roulette wheel selection strategy.
+
+        :return: A set of the fittest selected individuals.
+        """
         fittest: list[Input] = []
         for _ in range(self.tournament_rounds):
             for _ in range(min(self.tournament_size, len(self.test_inputs) - len(fittest))):

72ba501c586e10d3f6b0db27838e58f5d95cc514
JaasperK <jasper.l.koehn@gmail.com>
2024-07-10 23:06:10 +0200

    Added test cases for new selecion strategies

---
+++ tests/test_truncation_selection.py
@@ -0,0 +1,30 @@
+import unittest
+
+from isla.fuzzer import GrammarFuzzer
+from isla.derivation_tree import DerivationTree
+from debugging_benchmark.calculator.calculator import calculator_grammar as grammar
+
+from evogfuzz.input import Input
+from evogfuzz.truncation_selection import Truncation
+
+
+class TruncationSelection(unittest.TestCase):
+    def test_selection(self):
+        print()
+        fuzzer = GrammarFuzzer(grammar)
+        test_inputs = set()
+        for idx, _ in enumerate(range(100)):
+            inp = Input(DerivationTree.from_parse_tree(fuzzer.fuzz_tree()))
+            inp.fitness = idx
+            test_inputs.add(inp)
+        for inp in test_inputs:
+            print(inp, "\t", inp.oracle, "\t", inp.fitness)
+
+        selected = Truncation(test_inputs).select_fittest_individuals()
+        print("______")
+        for inp in selected:
+            print(inp, "\t", inp.oracle, "\t", inp.fitness)
+
+
+if __name__ == "__main__":
+    unittest.main()

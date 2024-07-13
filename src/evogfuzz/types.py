18a2632793bbfa6397878a3cc0d634fc256e6011
JaasperK <jasper.l.koehn@gmail.com>
2024-07-13 23:58:33 +0200

    removed bugs in types.py

--- src/evogfuzz/types.py
+++ src/evogfuzz/types.py
@@ -16,19 +16,13 @@ class GrammarType(Enum):
     def __str__(self):
         return self.value
 
+
 class Strategy(Enum):
-<<<<<<< HEAD
     TOURNAMENT = 0
     TRUNCATION = 1
     ROULETTE = 2
     RANK = 3
     STOCHASTIC_UNIVERSAL_SAMPLING = 4
-=======
-    TOURNAMENT = "Tournament"
-    TRUNCATION = "Truncation"
-    ROULETTE = "Roulette"
-    RANK = "Rank"
->>>>>>> 5157a0964daf4a86616a5ee797fae697745a39c1
 
     def __str__(self) -> str:
         return self.value
